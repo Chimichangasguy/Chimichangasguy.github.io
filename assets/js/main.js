171d84a3b8bab5ed1facb4d0933a82a747571764
Chimichangasguy <59568767+Chimichangasguy@users.noreply.github.com>
2024-08-15 13:57:08 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,3 +121,22 @@
 		});
 
 })(jQuery);
+
+// script.js
+
+// Get the button
+const goToTopBtn = document.getElementById('goToTopBtn');
+
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        goToTopBtn.style.display = 'block';
+    } else {
+        goToTopBtn.style.display = 'none';
+    }
+};
+
+// Scroll to the top of the document when the button is clicked
+goToTopBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
